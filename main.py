70148f2daad5f1330ca052f100078dd20c2928e2
stricklandrw <stricklandrw@gmail.com>
2026-02-16 18:33:00 -0500

    Ch1:LLMs, L5:User Input complete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,11 +11,16 @@ def main():
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY environment variable not found")
     client = genai.Client(api_key=api_key)
-    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+#   Hardcoded user prompt for testing - replace with argparse or other input method as needed
+#   user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=user_prompt)
+    parser = argparse.ArgumentParser(description="Generate content using Gemini API")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
 
-    print(f"User prompt: {user_prompt}")
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+
+    print(f"User prompt: {args.user_prompt}")
     if response.usage_metadata is not None:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
     else:

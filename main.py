893a9bd36a36dd77806a7d5a0120b2a5628d9571
stricklandrw <stricklandrw@gmail.com>
2026-02-17 08:29:53 -0500

    Ch1:LLMs, L7:Verbose Ouput complete

--- main.py
+++ main.py
@@ -17,14 +17,15 @@ def main():
 
     parser = argparse.ArgumentParser(description="Generate content using Gemini API")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
 
-    print(f"User prompt: {args.user_prompt}")
     if response.usage_metadata is not None:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+        if args.verbose is True:
+            print(f"User prompt: {args.user_prompt}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
     else:
         raise RuntimeError("Response usage metadata is None - Likely an error in the API response")
     print(f"Response:\n{response.text}")

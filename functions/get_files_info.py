b3680c81b4f5fe288d76f11145fe64e1c5cc428b
stricklandrw <stricklandrw@gmail.com>
2026-02-17 14:21:45 -0500

    Ch2:, L2: Get Files complete

---
+++ functions/get_files_info.py
@@ -0,0 +1,18 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    working_dir_abs = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+    # Will be True or False
+    print(f'Result for {directory} directory:')
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+    if not valid_target_dir:
+        return print(f'    Error: Cannot list "{directory}" as it is outside the permitted working directory')
+    if not os.path.exists(target_dir):
+        return print(f'Error: "{directory}" is not a directory')
+    try:
+        for file in os.listdir(target_dir):
+            file_path = os.path.join(target_dir, file)
+            print(f"  - {file}: file_size={os.path.getsize(file_path)} bytes, is_dir={os.path.isdir(file_path)}")
+    except Exception as e:
+        print(f'Error: An error occurred while listing files in "{directory}": {str(e)}')

03329c081f7ea3f46815af603ae0e7134c08d959
stricklandrw <stricklandrw@gmail.com>
2026-02-24 14:57:11 -0500

    Ch3:, L1: System Prompt complete

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-import argparse
+from prompts import system_prompt
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -15,13 +16,20 @@ def main():
 #   Hardcoded user prompt for testing - replace with argparse or other input method as needed
 #   user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
+    model_name = "gemini-2.5-flash"
     parser = argparse.ArgumentParser(description="Generate content using Gemini API")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+    response = client.models.generate_content(
+        model=model_name,
+        contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt,
+            temperature=0)
+        )
 
     if response.usage_metadata is not None:
         if args.verbose is True:
